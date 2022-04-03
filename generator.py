b5581d23360e643ef3d17481b0b92341e81fe2c3
hardik singh <realhardik18@gmail.com>
2022-04-03 16:25:10 +0530

    added all the files
    file to generate screenshot and file for the bot

---
+++ generator.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import time
+import pytesseract
+from msedge.selenium_tools import EdgeOptions
+from msedge.selenium_tools import Edge
+import credentials  # this is a file with the sensitive credentials and paths
+
+
+def Screenshoter():
+    edge_options = EdgeOptions()
+    edge_options.use_chromium = True
+    edge_options.add_argument('headless')
+    edge_options.add_argument('disable-gpu')
+    edge_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+    driver = Edge(executable_path=credentials.webdrive_path,
+                  options=edge_options)
+
+    driver.get('https://www.reddit.com/r/place/?cx=307&cy=315&px=100')
+    time.sleep(5)
+    driver.get_screenshot_as_file('yo.png')
+    driver.quit()
---
+++ main.py
@@ -0,0 +1,31 @@
+import discord
+import time
+from discord.ext import commands
+import credentials
+from generator import Screenshoter
+from PIL import Image
+import datetime
+
+client = commands.Bot(command_prefix="i ")
+client.remove_command('help')
+
+
+@client.event
+async def on_ready():
+    print("im alive and working!!(logged in as {0.user})".format(client))
+    channle = await client.fetch_channel(credentials.channle_id)
+    while True:
+        Screenshoter()
+        im = Image.open('yo.png')
+        im = im.crop((0, 100, im.width-10, im.height))
+        im.save('yo.png')
+        await channle.send(f'our region as of {datetime.datetime.now()}', file=discord.File("yo.png"))
+        print('sent ss')
+        time.sleep(180)
+
+
+@client.command()
+async def test(ctx):
+    await ctx.send("yes chef")
+
+client.run(credentials.bot_token)
